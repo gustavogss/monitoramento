220242cbfa439c6940c0831da3834053b2b754ff
gustavogss <gustavogss.dev@gmail.com>
2023-04-03 02:11:55 -0300

    Inciando projeto

---
+++ site.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Gustavo Souza"
+
+	fmt.Println("Olá sr.", nome, " - Escolha uma das opções abaixo")
+	fmt.Println("1 - Iniciar Monitoramento:")
+	fmt.Println("2 - Exibir Logs:")
+	fmt.Println("0 - Sair do Programa:")
+}
