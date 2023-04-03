596bd1686210029cdb2aeac2f56c1c83322147c3
gustavogss <gustavogss.dev@gmail.com>
2023-04-03 02:59:03 -0300

    Estruturando o projeto

--- site.go
+++ site.go
@@ -3,10 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	nome := "Gustavo Souza"
+	exibiSaudacoes()
+	exibiMenu()
+	exibiOpcoes()
+}
 
-	fmt.Println("Olá sr.", nome, " - Escolha uma das opções abaixo")
+func exibiSaudacoes() {
+	nome := "Gustavo Souza"
+	fmt.Println("Olá sr.", nome)
+	fmt.Println("Escolha uma das opções abaixo")
+}
+
+func exibiMenu() {
 	fmt.Println("1 - Iniciar Monitoramento:")
 	fmt.Println("2 - Exibir Logs:")
 	fmt.Println("0 - Sair do Programa:")
+}
+
+func leOpcao() int {
+	var opcaoLida int
+	fmt.Scan(&opcaoLida)
+	fmt.Println("A opção escolhida foi: ", opcaoLida)
+	return opcaoLida
+}
+func exibiOpcoes() {
+	comando := leOpcao()
+
+	switch comando {
+	case 0:
+		fmt.Println("Saindo do programa...")
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	default:
+		fmt.Println("Não existe essa opção")
+	}
 }
